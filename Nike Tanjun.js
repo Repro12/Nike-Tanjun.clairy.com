469572545154b7950922d07963040e1b0783482c
Repro12 <111174041+Repro12@users.noreply.github.com>
2022-08-14 05:06:31 -0700

    Add files via upload

---
+++ Nike Tanjun.js
@@ -0,0 +1,11 @@
+function myntra(){
+    window.location.href= "https://www.myntra.com/casual-shoes/nike/nike-men-black-tanjun-sneakers/16046018/buy?gclid=CjwKCAjwi8iXBhBeEiwAKbUofRfiPxbXZ2_HS5FaksPU8K53Ah4tG-AhX47aq5mQXhfJepj3tagbcxoCFmYQAvD_BwE";
+};
+
+function ajio(){
+    window.location.href= "https://www.ajio.com/nike-tanjun-lace-up-casual-shoes/p/469034075_black?gclid=CjwKCAjwi8iXBhBeEiwAKbUofaEt4XTa13J_RcFKb_xf4mDfpIKNTpoQRLss9PtdZMQ0LexlwO77PxoCC4AQAvD_BwE#gmf";
+};
+
+function nike(){
+    window.location.href= "https://www.nike.com/in/t/defy-all-day-training-shoe-LtD2G1/DJ1196-002";
+}
